b2ad929ed69549be63ce83b35648552c0cdbdd9e
SlimeB <gab.lacunza@gmail.com>
2020-11-09 10:55:36 -0300

    ejercicio 01

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-print("i hate u")
+/*proyecto: hacer un sudoku de consola
+paso 1: hacer que se impriman las plantillas
+paso 2: implementar comando para escribir el numero
+paso 3: imprimir pistas y hacerlas fijas*/
